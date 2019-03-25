646d00416c2e146a46f041f47760ef6cb5e69127
Alex Abreu <alex.abreu@sendgrid.com>
2019-03-24 19:37:37 -0600

    Updates

---
+++ precache-manifest.16bd0bd108ba5b43f956b2bc0d1d7e0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08f05785a7989a9d0b0e",
+    "url": "/pizza-bot/static/js/runtime~main.a6fa4f19.js"
+  },
+  {
+    "revision": "f53929f35890b305a540",
+    "url": "/pizza-bot/static/js/main.a92eb907.chunk.js"
+  },
+  {
+    "revision": "93bab71e6397b14a9b78",
+    "url": "/pizza-bot/static/js/2.ca947b24.chunk.js"
+  },
+  {
+    "revision": "f53929f35890b305a540",
+    "url": "/pizza-bot/static/css/main.9061d169.chunk.css"
+  },
+  {
+    "revision": "261f2c900ef59925e9008e25bdaaaed5",
+    "url": "/pizza-bot/index.html"
+  }
+];
